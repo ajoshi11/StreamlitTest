9f5bceea80833fb9628df2189e36f1161c5c12c3
ajoshi11 <atul.uhc@gmail.com>
2024-08-27 20:16:57 +0530

    Add files via upload

---
+++ testStreamlit.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+
+# Set the title of the Streamlit app
+st.title("Simple Hello App")
+
+# Display a text label
+st.write("Hello")
